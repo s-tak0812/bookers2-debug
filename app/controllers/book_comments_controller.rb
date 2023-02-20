b22a61f65b433bd82266e9ef2000307ed9e5b3a6
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-20 08:48:45 +0000

    [Update]model,controller

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,17 @@
 class BookCommentsController < ApplicationController
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:book_comments).permit(:comment)
+  end
+
 end
