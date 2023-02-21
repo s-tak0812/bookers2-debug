74c4fc8efaa2d4028a09d9a45b47d2cc55849c23
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-21 12:32:32 +0000

    [Add]フォロー機能

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,6 @@
 class Relationship < ApplicationRecord
-
+  # 中間モデル
+  # belongs_to :仮のcolomn名, class_name: "xxx"でxxxモデルを参照
   belongs_to :follower, class_name: "User"
 
   belongs_to :followed, class_name: "User"
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+  # 中間モデル
+  #1⃣ has_many :associationが繋がっているtable名(適当)-A,class_name:"中間table名",foreign_key:"外部key"
+  #2⃣ has_many :架空のtable名(.で繋げられる),through::-A,source::参照するcolumn名
+  # 1⃣-2⃣
+  # 1⃣
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 2⃣
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   has_one_attached :profile_image
 
@@ -18,4 +27,20 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  # followしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  # followを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  # followしているかの判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
