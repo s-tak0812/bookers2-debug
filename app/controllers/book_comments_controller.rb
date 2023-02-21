286a9895bab95f0c8b0bd6e1c6105f840113630b
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-21 07:33:15 +0000

    [Fix]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,14 +3,8 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
-    else
-      # @book_id = BookComment.find_by(book_id: book.id)
-      # @user = @book_id.user
-      # @book = Book.new
-      redirect_to request.referer
-    end
+    comment.save
+    redirect_to request.referer
   end
 
   def destroy
