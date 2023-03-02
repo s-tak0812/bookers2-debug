dd0d45be9b5f10a79cf1198e44e419608bee5078
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-03-02 06:18:18 +0000

    [Fix]chart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,8 @@ class UsersController < ApplicationController
     @last_7_days = (@user.books.created_last_7_days.count).to_i
     @before_last_week = (@user.books.created_before_last_week.count).to_i
 
-    @before_2days = (@user.books.created_2days_ago.count).to_i
-    @before_3days = (@user.books.created_3days_ago.count).to_i
-    @before_4days = (@user.books.created_4days_ago.count).to_i
-    @before_5days = (@user.books.created_5days_ago.count).to_i
-    @before_6days = (@user.books.created_6days_ago.count).to_i
-
     @books_shown = @user.books.page(params[:page])
+
   end
 
   def index
@@ -42,20 +37,10 @@ class UsersController < ApplicationController
 
   def search
     @user = User.find(params[:user_id])
-    @books = @user.books
-    if params[:created_at].nil?
-      @results = "本がありません"
-    else
-      created_at = params[:created_at]
-      @results = @books.where(['created_at LIKE ? ', "#{created_at}%"]).count
-    end
+    books = @user.books
+    @created_at = params[:created_at]
+    @results = books.where('created_at LIKE ? ', "#{@created_at}"+"%").count
   end
-
-
-
-
-
-
 
 
   private
